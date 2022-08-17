45b020ec6325cb159c3d6c74df371d732374d6c8
Shubham Krishna <shubham.krishna@ml6.eu>
2022-08-17 11:19:31 +0200

    Add Writing to PubSub and formatting

---
+++ write_to_pubsub_pipeline/config.py
@@ -0,0 +1,7 @@
+PROJECT_ID = "apache-beam-testing"
+# PubSub Topic ID
+TOPIC_ID = f"projects/{PROJECT_ID}/topics/newsgroup-dataset"
+JOB_NAME = "write-to-pub-sub"
+NUM_WORKERS = 1
+MAX_NUM_WORKERS = 15
+TABLE_SCHEMA = None
